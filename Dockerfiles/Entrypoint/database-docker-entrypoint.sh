e56da30bf30e84dc179665f1e264a4a699d9e6b6
SABBiR <team.cloudboost@gmail.com>
2022-11-04 11:46:06 +0600

    Creating Database Container

--- Dockerfiles/Entrypoint/database-docker-entrypoint.sh
+++ Dockerfiles/Entrypoint/database-docker-entrypoint.sh
@@ -2,26 +2,26 @@
 sed -i 's,bind-address.*,bind-address = 0.0.0.0,g' /etc/mysql/mysql.conf.d/mysqld.cnf;
 # Port Changing script should be added
 /etc/init.d/mysql start;
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH caching_sha2_password BY '${MYSQL_ROOT_PASSWORD}';"
-mysql -uroot -p${MYSQL_ROOT_PASSWORD} -e "CREATE DATABASE  IF NOT EXISTS $APP_DB;";
-mysql -uroot -p${MYSQL_ROOT_PASSWORD} -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';";
-mysql -uroot -p${MYSQL_ROOT_PASSWORD} -e "GRANT ALL ON $APP_DB.* TO '${MYSQL_USER}'@'%';";
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH caching_sha2_password BY '${MYSQL_ROOT_PASSWORD}';" > /dev/null
+mysql -uroot -p${MYSQL_ROOT_PASSWORD} -e "CREATE DATABASE  IF NOT EXISTS $APP_DB;"; > /dev/null
+mysql -uroot -p${MYSQL_ROOT_PASSWORD} -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"; > /dev/null
+mysql -uroot -p${MYSQL_ROOT_PASSWORD} -e "GRANT ALL ON $APP_DB.* TO '${MYSQL_USER}'@'%';"; > /dev/null
 
 
 ## DATABASE CHECKING BEFORE IMPORT
 
-TABLE=verify_users
-SQL_EXISTS=$(printf 'SHOW TABLES LIKE "%s"' "$TABLE")
-echo "Checking if table <$TABLE> exists ..."
 
-# Check if table exists
-if [[ $(mysql -u ${MYSQL_USER} -p${MYSQL_PASSWORD} -e "$SQL_EXISTS" $APP_DB) ]]
+if ! [ -f /var/lib/mysql/$APP_DB/verify_users_671.sdi ];
+
 then
-    echo "Database Already Imported";
+  echo "Database needs to be imported";
+  mysql -u${MYSQL_USER} -p${MYSQL_PASSWORD} $APP_DB < /glue/glue_dump.sql;
+  echo "Completed";
+
 else
-        echo "Table is empty, Importing Started. Please wait .....";
-        mysql -u${MYSQL_USER} -p${MYSQL_PASSWORD} $APP_DB < /glue/glue_dump.sql; 
-        echo "Database Import Completed";
+
+ echo "Already Imported";
+
 fi
 
 tail -f /dev/null
