5c5bff41b37d2d7d1f38f2441d70c0b52e634e65
sabbirrahman8 <team.cloudboost@gmail.com>
2022-11-08 09:53:19 +0600

    Update frontend1 docker file

--- Dockerfiles/Entrypoint/frontend-1-docker-entrypoint.sh
+++ Dockerfiles/Entrypoint/frontend-1-docker-entrypoint.sh
@@ -10,4 +10,4 @@ sed -i "s,DB_USERNAME.*,DB_USERNAME=$APP_DB_USER,g" /public_html/$REPO_PROJECT_F
 sed -i "s,DB_PASSWORD.*,DB_PASSWORD=$APP_DB_PASS,g" /public_html/$REPO_PROJECT_FOLDER/.env;
 sed -i "s,DB_HOST.*,DB_HOST=$APP_DB_HOST,g" /public_html/$REPO_PROJECT_FOLDER/.env;
 sed -i "s,DB_PORT.*,DB_PORT=$APP_DB_PORT,g" /public_html/$REPO_PROJECT_FOLDER/.env;
-nginx -g 'daemon off;';
+#nginx -g 'daemon off;';
