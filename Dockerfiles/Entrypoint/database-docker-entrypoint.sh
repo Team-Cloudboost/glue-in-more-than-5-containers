17c903bb1e5c7ef4d73738e48502124fa20315c1
sabbirrahman8 <team.cloudboost@gmail.com>
2022-11-08 10:48:22 +0600

    Update env file

--- Dockerfiles/Entrypoint/database-docker-entrypoint.sh
+++ Dockerfiles/Entrypoint/database-docker-entrypoint.sh
@@ -5,8 +5,8 @@ sed -i "s,# port.*,port = $APP_DB_PORT,g" /etc/mysql/mysql.conf.d/mysqld.cnf;
 /etc/init.d/mysql start;
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH caching_sha2_password BY '${MYSQL_ROOT_PASSWORD}';"
 mysql -uroot -p${MYSQL_ROOT_PASSWORD} -e "CREATE DATABASE  IF NOT EXISTS $APP_DB;"; 
-mysql -uroot -p${MYSQL_ROOT_PASSWORD} -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"; 
-mysql -uroot -p${MYSQL_ROOT_PASSWORD} -e "GRANT ALL ON $APP_DB.* TO '${MYSQL_USER}'@'%';";
+mysql -uroot -p${MYSQL_ROOT_PASSWORD} -e "CREATE USER IF NOT EXISTS '${APP_DB_USER}'@'%' IDENTIFIED BY '${APP_DB_PASS}';"; 
+mysql -uroot -p${MYSQL_ROOT_PASSWORD} -e "GRANT ALL ON $APP_DB.* TO '${APP_DB_USER}'@'%';";
 
 
 ## DATABASE CHECKING BEFORE IMPORT
@@ -15,7 +15,7 @@ if ! [ -f /var/lib/mysql/$APP_DB/verify_users_* ];
 
 then
    echo  $(date +"%T") "Database needs to be imported";
-   mysql -u${MYSQL_USER} -p${MYSQL_PASSWORD} $APP_DB < /glue/glue_dump.sql;
+   mysql -u${APP_DB_USER} -p${APP_DB_PASS} $APP_DB < /glue/glue_dump.sql;
    echo  $(date +"%T") "Database Import is being Completed";
 
 else
